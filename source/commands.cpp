840abead923daa72d66bb64ff2420f6f0da5d80f
anki <an.kiselev.2008@gmail.com>
2025-10-07 17:43:34 +0300

    switch for CmdArgsCount

--- source/commands.cpp
+++ source/commands.cpp
@@ -1,28 +1,22 @@
 #include "commands.h"
 
+// в массив с командами вписывать колво аргументов, потом циклом проходить по нему и смотреть аргументы; либо пронумеровать команды так, чтобы они совпадали с индексами массива
 int CmdArgsCount(Command_t command)
 {
-    // switch
-    if (command == CMD_PUSH)
-        return 1;
-    if (command == CMD_POPR)
-        return 1;
-    if (command == CMD_PUSHR)
-        return 1;
-    if (command == CMD_JMP)
-        return 1;
-    if (command == CMD_JB)
-        return 1;
-    if (command == CMD_JBE)
-        return 1;
-    if (command == CMD_JA)
-        return 1;
-    if (command == CMD_JAE)
-        return 1;
-    if (command == CMD_JE)
-        return 1;
-    if (command == CMD_JNE)
-        return 1;
+    switch (command)
+    {
+        case CMD_PUSH:  return 1;
+        case CMD_POPR:  return 1;
+        case CMD_PUSHR: return 1;
+        case CMD_JMP:   return 1;
+        case CMD_JB:    return 1;
+        case CMD_JBE:   return 1;
+        case CMD_JA:    return 1;
+        case CMD_JAE:   return 1;
+        case CMD_JE:    return 1;
+        case CMD_JNE:   return 1;
+        default:        return 0;
+    }
 
     return 0;
 }
