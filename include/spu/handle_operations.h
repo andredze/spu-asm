bfd24845cd4c741725ea457816a9784f6283cd17
anki <an.kiselev.2008@gmail.com>
2025-10-18 23:35:34 +0300

    handle_operations

---
+++ include/spu/handle_operations.h
@@ -0,0 +1,73 @@
+#ifndef HANDLE_OPERATIONS_H
+#define HANDLE_OPERATIONS_H
+
+#include <TXLib.h>
+#include <math.h>
+#include <windows.h>
+#include "stack.h"
+#include "input.h"
+#include "spu_types.h"
+
+typedef enum MathErr {
+    MATH_SUCCESS,
+    MATH_DIVISION_BY_ZERO,
+    MATH_STACK_ERROR,
+} MathErr_t;
+
+typedef enum HandleOpErr {
+    HANDLE_OP_SUCCESS,
+    HANDLE_OP_DIVISION_BY_ZERO,
+    HANDLE_OP_STACK_ERROR,
+    HANDLE_OP_INVALID_JUMP_ARG,
+    HANDLE_OP_INVALID_REG_ARG,
+    HANDLE_OP_INVALID_RAM_ARG,
+    HANDLE_OP_INPUT_ERROR,
+    HANDLE_OP_BREAK_LOOP
+} HandleOpErr_t;
+
+typedef struct CalcData {
+    HandleOpErr_t number1;
+    HandleOpErr_t number2;
+    HandleOpErr_t result;
+} CalcData_t;
+
+MathErr_t Add(CalcData_t* calc_data);
+
+MathErr_t Sub(CalcData_t* calc_data);
+
+MathErr_t Mul(CalcData_t* calc_data);
+
+MathErr_t Div(CalcData_t* calc_data);
+
+MathErr_t Mod(CalcData_t* calc_data);
+
+int Jump(Proc_t* proc_data, int new_cmd_count);
+
+HandleOpErr_t HandleHLT   (Proc_t* proc_data);
+HandleOpErr_t HandlePUSH  (Proc_t* proc_data);
+HandleOpErr_t HandleOUT   (Proc_t* proc_data);
+HandleOpErr_t HandleIN    (Proc_t* proc_data);
+HandleOpErr_t HandleCALL  (Proc_t* proc_data);
+HandleOpErr_t HandleRET   (Proc_t* proc_data);
+HandleOpErr_t HandleADD   (Proc_t* proc_data);
+HandleOpErr_t HandleSUB   (Proc_t* proc_data);
+HandleOpErr_t HandleMUL   (Proc_t* proc_data);
+HandleOpErr_t HandleDIV   (Proc_t* proc_data);
+HandleOpErr_t HandleMOD   (Proc_t* proc_data);
+HandleOpErr_t HandleSQRT  (Proc_t* proc_data);
+HandleOpErr_t HandleSQR   (Proc_t* proc_data);
+HandleOpErr_t HandleJMP   (Proc_t* proc_data);
+HandleOpErr_t HandleJB    (Proc_t* proc_data);
+HandleOpErr_t HandleJBE   (Proc_t* proc_data);
+HandleOpErr_t HandleJA    (Proc_t* proc_data);
+HandleOpErr_t HandleJAE   (Proc_t* proc_data);
+HandleOpErr_t HandleJE    (Proc_t* proc_data);
+HandleOpErr_t HandleJNE   (Proc_t* proc_data);
+HandleOpErr_t HandlePUSHR (Proc_t* proc_data);
+HandleOpErr_t HandlePOPR  (Proc_t* proc_data);
+HandleOpErr_t HandlePUSHM (Proc_t* proc_data);
+HandleOpErr_t HandlePOPM  (Proc_t* proc_data);
+HandleOpErr_t HandleDRAW  (Proc_t* proc_data);
+HandleOpErr_t DrawInWindow(Proc_t* proc_data);
+
+#endif /* HANDLE_OPERATIONS_H */
---
+++ source/spu/handle_operations.cpp
@@ -0,0 +1,464 @@
+#include "handle_operations.h"
+
+#define DECLARE_HANDLE_JUMP_IF(comp_oper, cmd_name)                     \
+    HandleOpErr_t Handle##cmd_name(Proc_t* proc_data)                   \
+    {                                                                   \
+        assert(proc_data != NULL);                                      \
+                                                                        \
+        int new_cmd_count = proc_data->code[proc_data->cmd_count++];    \
+        int number1 = 0;                                                \
+        int number2 = 0;                                                \
+        if (StackPop(&proc_data->stack, &number2))                      \
+        {                                                               \
+            return HANDLE_OP_STACK_ERROR;                               \
+        }                                                               \
+        if (StackPop(&proc_data->stack, &number1))                      \
+        {                                                               \
+            return HANDLE_OP_STACK_ERROR;                               \
+        }                                                               \
+        DPRINTF("\t" #cmd_name ": (%d " #comp_oper " %d) == %d\n",      \
+                number1, number2, number1 comp_oper number2);           \
+        if (!(number1 comp_oper number2))                               \
+        {                                                               \
+            DPRINTF("\tJMP rejected\n");                                \
+            return HANDLE_OP_SUCCESS;                                   \
+        }                                                               \
+        if (Jump(proc_data, new_cmd_count))                             \
+        {                                                               \
+            return HANDLE_OP_INVALID_ARG;                               \
+        }                                                               \
+        return HANDLE_OP_SUCCESS;                                       \
+    }
+
+DECLARE_HANDLE_JUMP_IF(<,  Jb);
+DECLARE_HANDLE_JUMP_IF(<=, Jbe);
+DECLARE_HANDLE_JUMP_IF(>,  Ja);
+DECLARE_HANDLE_JUMP_IF(>=, Jae);
+DECLARE_HANDLE_JUMP_IF(==, Je);
+DECLARE_HANDLE_JUMP_IF(!=, Jne);
+
+#define DECLARE_HANDLE_MATH_OP(cmd_name, calculate)                             \
+    HandleOpErr_t Handle##cmd_name(Proc_t* proc_data)                           \
+    {                                                                           \
+        assert(proc_data->stack.size >= 2);                                     \
+                                                                                \
+        CalcData_t calc_data = {};                                              \
+        if (StackPop(&proc_data->stack, &calc_data.number1) != STACK_SUCCESS)   \
+        {                                                                       \
+            return HANDLE_OP_STACK_ERROR;                                       \
+        }                                                                       \
+        if (StackPop(&proc_data->stack, &calc_data.number2) != STACK_SUCCESS)   \
+        {                                                                       \
+            return HANDLE_OP_STACK_ERROR;                                       \
+        }                                                                       \
+        if (calculate(&calc_data) != MATH_SUCCESS)                              \
+        {                                                                       \
+            return HANDLE_OP_DIVISION_BY_ZERO;                                  \
+        }                                                                       \
+        if (StackPush(&proc_data->stack, calc_data.result) != STACK_SUCCESS)    \
+        {                                                                       \
+            return HANDLE_OP_STACK_ERROR;                                       \
+        }                                                                       \
+        return HANDLE_OP_SUCCESS;                                               \
+    }
+
+DECLARE_HANDLE_MATH_OP(Add, Add); // +
+DECLARE_HANDLE_MATH_OP(Sub, Sub); // -
+DECLARE_HANDLE_MATH_OP(Mul, Mul);
+DECLARE_HANDLE_MATH_OP(Div, Div); // / , {if (b == 0) Error();}
+DECLARE_HANDLE_MATH_OP(Mod, Mod);
+
+HandleOpErr_t HandleHLT(Proc_t* proc_data)
+{
+    return HANDLE_OP_BREAK_LOOP;
+}
+
+HandleOpErr_t HandleSqrt(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int number = 0;
+    if (StackPop(&proc_data->stack, &number) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    int value = (int) sqrt((double) number);
+    if (StackPush(&proc_data->stack, value) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    DPRINTF("\tSQRT: sqrt(%d) = %d\n", number, value);
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandleSqr(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int number = 0;
+    if (StackPop(&proc_data->stack, &number) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    int value = number * number;
+    if (StackPush(&proc_data->stack, value) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    DPRINTF("\tSQR: %d ** 2 = %d\n", number, value);
+
+    return HANDLE_OP_SUCCESS;
+}
+
+MathErr_t Add(CalcData_t* calc_data)
+{
+    assert(calc_data != NULL);
+    assert(calc_data->result == 0);
+
+    calc_data->result = calc_data->number1 + calc_data->number2;
+    DPRINTF("\tADD: %d + %d = %d\n", calc_data->number2, calc_data->number1, calc_data->result);
+
+    return MATH_SUCCESS;
+}
+
+MathErr_t Sub(CalcData_t* calc_data)
+{
+    assert(calc_data != NULL);
+    assert(calc_data->result == 0);
+
+    calc_data->result = calc_data->number2 - calc_data->number1;
+    DPRINTF("\tSUB: %d - %d = %d\n", calc_data->number2, calc_data->number1, calc_data->result);
+
+    return MATH_SUCCESS;
+}
+
+MathErr_t Mul(CalcData_t* calc_data)
+{
+    assert(calc_data != NULL);
+    assert(calc_data->result == 0);
+
+    calc_data->result = calc_data->number1 * calc_data->number2;
+    DPRINTF("\tMUL: %d * %d = %d\n", calc_data->number2, calc_data->number1, calc_data->result);
+
+    return MATH_SUCCESS;
+}
+
+MathErr_t Div(CalcData_t* calc_data)
+{
+    assert(calc_data != NULL);
+    assert(calc_data->result == 0);
+
+    if (calc_data->number1 == 0)
+    {
+        printf("Can not divide by zero\n");
+        return MATH_DIVISION_BY_ZERO;
+    }
+    calc_data->result = calc_data->number2 / calc_data->number1;
+    DPRINTF("\tDIV: %d / %d = %d\n", calc_data->number2, calc_data->number1, calc_data->result);
+
+    return MATH_SUCCESS;
+}
+
+MathErr_t Mod(CalcData_t* calc_data)
+{
+    assert(calc_data != NULL);
+    assert(calc_data->result == 0);
+
+    if (calc_data->number1 == 0)
+    {
+        printf("Can not divide by zero\n");
+        return MATH_DIVISION_BY_ZERO;
+    }
+    calc_data->result = calc_data->number2 % calc_data->number1;
+    DPRINTF("\tMOD: %d %% %d = %d\n", calc_data->number2, calc_data->number1, calc_data->result);
+
+    return MATH_SUCCESS;
+}
+
+HandleOpErr_t HandleOut(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int result = 0;
+    StackErr_t pop_return = StackPop(&proc_data->stack, &result);
+
+    if (pop_return == STACK_SIZE_IS_ZERO)
+    {
+        return HANDLE_OP_SUCCESS;
+    }
+    if (pop_return != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    printf("ANSWER = %d\n", result);
+    fflush(stdout);
+
+    DPRINTF("\tOUT: %d\n", result);
+
+    return 0;
+}
+
+HandleOpErr_t HandlePopr(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int index = proc_data->code[proc_data->cmd_count++];
+    int value = 0;
+    StackErr_t pop_return = StackPop(&proc_data->stack, &value);
+
+    if (pop_return == STACK_SIZE_IS_ZERO)
+    {
+        return HANDLE_OP_SUCCESS;
+    }
+    if (pop_return != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+
+    if (index < 0 || index > 7)
+    {
+        DPRINTF("Invalid register index given\n");
+        return HANDLE_OP_INVALID_REG_ARG;
+    }
+    proc_data->regs[index] = value;
+
+    DPRINTF("\tPOPR: poped %d\n"
+            "\t\treg[%d] = %d (R%cX)\n",
+            value, index, value, index + 'A');
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandlePushr(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int index = proc_data->code[proc_data->cmd_count++];
+
+    if (index < 0 || index > 7)
+    {
+        DPRINTF("Invalid register index given\n");
+        return HANDLE_OP_INVALID_REG_ARG;
+    }
+    if (StackPush(&proc_data->stack, proc_data->regs[index]) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+
+    DPRINTF("\tPUSHR: reg[%d] = %d (R%cX)\n"
+            "\t\tpushed %d\n",
+            index, proc_data->regs[index],
+            index + 'A', proc_data->regs[index]);
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandleIn(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int value = 0;
+    printf("Enter int value: ");
+    if (scanf("%d", &value) != 1)
+    {
+        printf("Input is wrong, try better :)\n");
+        return HANDLE_OP_INPUT_ERROR;
+    }
+
+#ifdef PROC_DEBUG
+    getchar();
+#endif /* PROC_DEBUG */
+
+    if (StackPush(&proc_data->stack, value) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    DPRINTF("\tIN: got and pushed %d\n", value);
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandleJmp(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int new_cmd_count = proc_data->code[proc_data->cmd_count++];
+
+    DPRINTF("\tJMP: ");
+    if (Jump(proc_data, new_cmd_count))
+    {
+        return HANDLE_OP_INVALID_JUMP_ARG;
+    }
+
+    return HANDLE_OP_SUCCESS;
+}
+
+int Jump(Proc_t* proc_data, int new_cmd_count)
+{
+    assert(proc_data != NULL);
+
+    if (new_cmd_count > proc_data->code_size || new_cmd_count < 0)
+    {
+        DPRINTF("Invalid argument for jump\n");
+        return 1;
+    }
+    proc_data->cmd_count = new_cmd_count;
+    DPRINTF("\t   jumping to %d\n", new_cmd_count);
+
+    return 0;
+}
+
+HandleOpErr_t HandleCall(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int new_cmd_count = proc_data->code[proc_data->cmd_count++];
+
+    if (StackPush(&proc_data->call_stack, proc_data->cmd_count) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+
+    DPRINTF("\tCALL: pushed %zu to call_stack\n", proc_data->cmd_count);
+
+    if (Jump(proc_data, new_cmd_count))
+    {
+        return HANDLE_OP_INVALID_JUMP_ARG;
+    }
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandleRet(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int call_address = -1;
+
+    if (StackPop(&proc_data->call_stack, &call_address) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+
+    DPRINTF("\tRET: returning to %d\n", call_address);
+
+    if (Jump(proc_data, call_address))
+    {
+        return HANDLE_OP_INVALID_JUMP_ARG;
+    }
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandlePushm(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int reg_number = proc_data->code[proc_data->cmd_count++];
+    int mem_addr = proc_data->regs[reg_number];
+    DPRINTF("\tPUSHM: address = regs[%d] (R%cX) = %d\n", reg_number, 'A' + reg_number, mem_addr);
+
+    if (!(mem_addr >= 0 && mem_addr < RAM_SIZE))
+    {
+        printf("Given address to pushm is too big\n");
+        return HANDLE_OP_INVALID_RAM_ARG;
+    }
+
+    int value = proc_data->ram[mem_addr];
+    if (StackPush(&proc_data->stack, value))
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    DPRINTF("\t\tpushed from ram[%d] = %d\n", mem_addr, value);
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandlePopm(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int reg_number = proc_data->code[proc_data->cmd_count++];
+    size_t mem_addr = proc_data->regs[reg_number];
+    DPRINTF("\tPOPM: address = regs[%d] (R%cX) = %zu\n", reg_number, 'A' + reg_number, mem_addr);
+
+    if (!(mem_addr <= MAX_RAM_ADDRESS && mem_addr < RAM_SIZE))
+    {
+        printf("Given address to pushm is too big\n");
+        return HANDLE_OP_INVALID_RAM_ARG;
+    }
+
+    int value = 0;
+
+    if (StackPop(&proc_data->stack, &value))
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+    proc_data->ram[mem_addr] = value;
+    DPRINTF("\t\tpoped to ram[%zu] = %d\n", mem_addr, value);
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandleDraw(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int sleep_time = proc_data->code[proc_data->cmd_count++];
+
+    for (size_t i = 0; i < RAM_SIZE; i++)
+    {
+        if (proc_data->ram[i] == 0) {
+            printf(" - "); }
+        else {
+            printf(" %c ", proc_data->ram[i]); }
+        if ((i + 1) % RAM_SIDE_SIZE == 0)
+        {
+            printf("\n");
+        }
+    }
+    printf("\n\n");
+    Sleep(sleep_time);
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t HandlePush(Proc_t* proc_data)
+{
+    assert(proc_data != NULL);
+
+    int value = proc_data->code[proc_data->cmd_count++];
+
+    DPRINTF("\tPUSH: %d\n", value);
+    if (StackPush(&proc_data->stack, value) != STACK_SUCCESS)
+    {
+        return HANDLE_OP_STACK_ERROR;
+    }
+
+    return HANDLE_OP_SUCCESS;
+}
+
+HandleOpErr_t DrawInWindow(Proc_t* proc_data)
+{
+    txCreateWindow(100, 100);
+    HDC dc = txDC();
+    txSetDefaults();
+    txSetColor(TX_WHITE, 0, dc);
+
+    COLORREF color = TX_WHITE;
+    for (int x_ram = 0; x_ram < RAM_SIDE_SIZE; x_ram++)
+    {
+        for (int y_ram = 0; y_ram < RAM_SIDE_SIZE; y_ram++)
+        {
+            color = TX_WHITE;
+            if (proc_data->ram[y_ram * RAM_SIDE_SIZE + x_ram] != 0)
+                color = TX_GREEN;
+            txSetPixel(x_ram, y_ram, color, dc);
+        }
+    }
+
+    txSaveImage("draw.bmp", dc);
+
+    return HANDLE_OP_SUCCESS;
+}
